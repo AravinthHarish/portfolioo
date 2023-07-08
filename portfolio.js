9ab80b63d8125aceb463a0538bfe917bbd2e264d
ARAVINTH.S <138973974+AravinthHarish@users.noreply.github.com>
2023-07-08 16:50:50 +0530

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+portfolio.js
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+  e.preventDefault();
+});
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "(Developer).";
+}
+
+
+
